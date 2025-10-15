f95a9d8e30b45bfcf0f4145b4dbcf01833e2a2cc
Maik28114 <bbb-maik@web.de>
2025-10-15 10:17:58 +0200

    Clean imports, ScrollToTop, 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -2,46 +2,47 @@
 import React from 'react'
 // Router-Komponenten importieren
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-// Layout-Komponente importieren
+
+// ===== COMPONENTS =====
 import Layout from './components/Layout'
-// Alle Seiten importieren
+import ScrollToTop from './components/ScrollToTop'
+
+// ===== PAGES =====
+import EmergencyPage from './pages/EmergencyPage'
 import HomePage from './pages/HomePage'
+import NotFoundPage from './pages/NotFoundPage'
 import SearchPage from './pages/SearchPage'
 import UploadPage from './pages/UploadPage'
-import EmergencyPage from './pages/EmergencyPage'
-import ScrollToTop from './components/ScrollToTop'
 
 // Haupt-App-Komponente
 function App() {
   return (
-    // BrowserRouter = Aktiviert Routing in der ganzen App
-    // Muss AUSSEN um alles sein
+    // BrowserRouter = Aktiviert Routing
     <BrowserRouter>
       
-      {/* Layout = Header + Content + Footer */}
-      {/* Bleibt auf allen Seiten gleich */}
+      {/* Scrollt automatisch nach oben bei Seitenwechsel */}
+      <ScrollToTop />
+      
+      {/* Layout = Header + Content + Footer (bleibt auf allen Seiten) */}
       <Layout>
         
         {/* Routes = Container für alle Routen */}
-        {/* Hier definierst du welche URL welche Seite zeigt */}
         <Routes>
           
-          {/* Route = Eine einzelne Route/Seite */}
-          {/* path="/" = URL-Pfad (Startseite) */}
-          {/* element={<HomePage />} = Welche Komponente anzeigen */}
+          {/* Startseite */}
           <Route path="/" element={<HomePage />} />
           
           {/* Suchseite */}
-          {/* URL: http://localhost:5173/search */}
           <Route path="/search" element={<SearchPage />} />
           
           {/* Upload-Seite */}
-          {/* URL: http://localhost:5173/upload */}
           <Route path="/upload" element={<UploadPage />} />
           
           {/* Notruf-Seite */}
-          {/* URL: http://localhost:5173/emergency */}
           <Route path="/emergency" element={<EmergencyPage />} />
+          
+          {/* 404 Seite (für alle anderen URLs) */}
+          <Route path="*" element={<NotFoundPage />} />
           
         </Routes>
         
